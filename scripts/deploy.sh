f1171988a2540bde7ccf4fd06f029956a4260e46
Sajay Antony <sajay@live.com>
2020-10-14 15:07:54 -0700

    Updating pages a9ceab758e128269e35e4172734dcfbbcafce95e

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,8 @@
 cd $(git rev-parse --show-toplevel)
 python3 ./get-tags.py
 
-
-git push origin :gh-pages
-git banch -D gh-pages
-git checkout -b gh-pages
 hugo -s $(git rev-parse --show-toplevel)/site -d ../docs
 hash=$(git rev-parse HEAD)
-git add -f ./docs
+git add ./docs
 git commit -m "Updating pages $hash"
-git push origin gh-pages
-git checkout main
+git push origin main 
