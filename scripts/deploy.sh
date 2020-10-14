a9ceab758e128269e35e4172734dcfbbcafce95e
Sajay Antony <sajay@live.com>
2020-10-14 14:55:20 -0700

    Fixing deploy to remove gh-pages history

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,10 @@ cd $(git rev-parse --show-toplevel)
 python3 ./get-tags.py
 
 
+git push origin :gh-pages
+git banch -D gh-pages
+git checkout -b gh-pages
 hugo -s $(git rev-parse --show-toplevel)/site -d ../docs
-git checkout gh-pages
 hash=$(git rev-parse HEAD)
 git add -f ./docs
 git commit -m "Updating pages $hash"
