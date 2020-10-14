1429207381daa1796fdad7824cfaf95b81c4c144
Sajay Antony <sajay@live.com>
2020-10-14 14:32:56 -0700

    Fixed deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh 
 
 cd $(git rev-parse --show-toplevel)
-hugo -s $(git rev-parse --show-toplevel) -d ../docs
+hugo -s $(git rev-parse --show-toplevel)/site -d ../docs
 git checkout gh-pages
 hash=$(git rev-parse HEAD)
-git add .
+git add -f ./docs
 git commit -m "Updating pages $hash"
 git push origin gh-pages
 git checkout main
